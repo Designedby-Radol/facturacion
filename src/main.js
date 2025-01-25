b97b5d6d769c1592d0c97633b9bf2865bdf42b88
Designedby-Radol <raulsantiagoramirez1@gmail.com>
2025-01-24 19:37:28 -0500

    refactor: :sparkles:

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
+import './components/user-form.js';
+import './components/product-selector.js';
+import './components/bill-summary.js';
+import './components/invoices-list.js';
